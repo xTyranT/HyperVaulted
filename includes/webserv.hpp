8bda4685d98cfa24f1a8aab3b351e2b0b305aa67
xTyranT <oufer.kamal@gmail.com>
2024-01-18 15:45:36 +0100

    made sure that the attribute names are valid, now nned to put the value of these attributes depending on what it is (integer, string, long, etc..)

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -8,30 +8,52 @@
 #include <cstdio>
 #include <fstream>
 #include <sstream>
+#include <typeinfo> 
 #include <stack>
 #include <vector>
 #include <map>
 
+#define PORT "port"
+#define HOST "host"
+#define ROOT "root"
+#define INDEX "index"
+#define SRVNAMES "server_name"
+#define ERRPAGES "error_pages"
+#define MAXBDSIZE "max_body_size"
+#define LOCATION "location"
+
+enum typeCode
+{
+    _int,
+    _string,
+    _vector,
+    _map,
+    _long,
+    _l_config,
+    _none
+};
+
 typedef struct l_config
 {
-    std::map<std::string, std::vector<std::string> > methods;
-    std::map<std::string, std::string> root;
-    std::map<std::string, std::vector<std::string> > indexes;
-    std::map<std::string, bool> autoIndex;
-    std::map<std::string, bool> upload;
-    std::map<std::string, bool> cgi;
-    std::map<std::string, std::string> uploadPath;
-    std::map<std::string, std::map<std::string, std::string> > cgiPaths;
+    std::map<std::string, void*> methods;     // std::vector<std::string>
+    std::map<std::string, void*> root;         // std::string
+    std::map<std::string, void*> indexes;     // std::vector<std::string>
+    std::map<std::string, void*> autoIndex;    // bool
+    std::map<std::string, void*> upload;       // bool
+    std::map<std::string, void*> cgi;          // bool
+    std::map<std::string, void*> uploadPath;   // std::string
+    std::map<std::string, void*> cgiPaths;    // std::map<std::string, std::string>
 } l_config;
 
 typedef struct s_config
 {
-    std::map<std::string, int> port;
-    std::map<std::string, std::string> host;
-    std::map<std::string, std::string> root;
-    std::map<std::string, std::vector<std::string> > indexes;
-    std::map<std::string, std::vector<std::string> > srvNames;
-    std::map<std::string, std::map<int, std::string> > errPages;
-    std::map<std::string ,long> maxBodySize;
-    std::map<std::string, l_config> location;
+    std::string server;
+    std::map<std::string, void*> port;         // int
+    std::map<std::string, void*> host;         // std::string
+    std::map<std::string, void*> root;         // std::string
+    std::map<std::string, void*> indexes;     // std::vector<std::string>
+    std::map<std::string, void*> srvNames;    // std::vector<std::string>
+    std::map<std::string, void*> errPages;    // std::map<int, std::string>
+    std::map<std::string, void*> maxBodySize;  // long
+    std::map<std::string, void*> location;     // l_config
 } s_config;
--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -1,94 +1,203 @@
 #include "../includes/webserv.hpp"
 
-
-
 void attribute_initializer(s_config& server)
 {
     /* server */
-    server.port.insert(std::pair<std::string, int>("port", -1));
-    server.host.insert(std::pair<std::string, std::string>("host", ""));
-    server.root.insert(std::pair<std::string, std::string>("root", ""));
-    server.indexes.insert(std::pair<std::string, std::vector<std::string> >("index", std::vector<std::string>()));
-    server.srvNames.insert(std::pair<std::string, std::vector<std::string> >("server_names", std::vector<std::string>()));
-    server.errPages.insert(std::pair<std::string, std::map<int, std::string> >("error_pages", std::map<int, std::string>()));
-    server.maxBodySize.insert(std::pair<std::string, long>("max_body_size", -1));
-        /* location */
-        l_config location;
-
-        location.methods.insert(std::pair<std::string, std::vector<std::string> >("methods", std::vector<std::string>()));
-        location.root.insert(std::pair<std::string, std::string>("root", ""));
-        location.indexes.insert(std::pair<std::string, std::vector<std::string> >("index", std::vector<std::string>()));
-        location.autoIndex.insert(std::pair<std::string, bool>("autoIndex", false));
-        location.upload.insert(std::pair<std::string, bool>("upload", false));
-        location.cgi.insert(std::pair<std::string, bool>("cgi", false));
-        location.uploadPath.insert(std::pair<std::string, std::string>("upload_path", ""));
-        location.cgiPaths.insert(std::pair<std::string, std::map<std::string, std::string> >("cgi_path", std::map<std::string, std::string>()));
-
-    server.location.insert(std::pair<std::string, l_config>("location", location));
+    server.server = "server";
+    server.port.insert(std::pair<std::string, void*>("port", NULL));
+    server.host.insert(std::pair<std::string, void*>("host", NULL));
+    server.root.insert(std::pair<std::string, void*>("root", NULL));
+    server.indexes.insert(std::pair<std::string, void*>("index", NULL));
+    server.srvNames.insert(std::pair<std::string, void*>("server_names", NULL));
+    server.errPages.insert(std::pair<std::string, void*>("error_pages", NULL));
+    server.maxBodySize.insert(std::pair<std::string, void*>("max_body_size", NULL));
+    server.location.insert(std::pair<std::string, void*>("location", NULL));
 }
 
-void set_attr_value(s_config& server, std::string& attr)
+std::string config_reader(char** argv)
 {
-    std::vector<std::string> attrs;
-
-    for(std::vector<std::string>::iterator i = attrs.begin(); i != attrs.end(); i++)
-    {
-        
-    }
-}
-
-std::ifstream& config_reader(char** argv)
-{
-    std::ifstream* file = new std::ifstream(argv[1]);
-    if (!file->is_open())
-    {
-        delete file;
+    std::ifstream file(argv[1]);
+    std::stringstream buffer;
+    if (!file.is_open())
         throw std::invalid_argument("no such file or permission denied");
-    }
-    return *file;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
 
-void server_block(std::ifstream& file)
+typeCode get_the_actual_type(std::string& attr)
+{
+    if (attr == PORT)
+        return _int;
+    if(attr == HOST || attr == ROOT)
+        return _string;
+    if(attr == INDEX || attr == SRVNAMES)
+        return _vector;
+    if(attr == ERRPAGES)
+       return _map;
+    if(attr == MAXBDSIZE)
+        return _long;
+    if(attr == LOCATION)
+        return _l_config;
+    return _none;
+}
+
+void* allocate_the_appropriate_type(std::string& attr)
+{
+    void* final_type = NULL;
+    typeCode code = get_the_actual_type(attr);
+    switch (code)
+    {
+        case _int:
+            int* integer = new (int);
+            final_type = integer;
+            break;
+        case _string:
+            std::string* string = new (std::string);
+            final_type = string;
+            break;
+        case _vector:
+            std::vector<std::string>* vector = new (std::vector<std::string>);
+            final_type = vector;
+            break;
+        case _map:
+            std::map<int, std::string>* map = new (std::map<int, std::string>);
+            final_type = vector;
+            break;
+        case _long:
+            long* lng = new (long);
+            final_type = lng;
+            break;
+        case _l_config:
+            l_config* location = new (l_config);
+            final_type = location;
+            break;
+        case _none:
+            throw std::invalid_argument(attr + " unknown variable");
+    }
+    return final_type;
+}
+
+void give_attr_value(s_config& server,std::string value)
+{
+    std::vector<std::map<std::string, void*> > srvVars;
+    std::vector<std::map<std::string, void*> >::iterator i;
+    std::map<std::string, void*>::iterator it;
+
+    srvVars.push_back(server.port);
+    srvVars.push_back(server.host);
+    srvVars.push_back(server.root);
+    srvVars.push_back(server.indexes);
+    srvVars.push_back(server.srvNames);
+    srvVars.push_back(server.errPages);
+    srvVars.push_back(server.maxBodySize);
+    srvVars.push_back(server.location);
+
+    for(i = srvVars.begin(); i != srvVars.end(); i++)
+    {
+        it = (*i).begin();
+        if (*(it->second) = )
+        {
+            it->second = allocate_the_appropriate_type(var.first);
+            break;
+        }
+    }
+}
+
+
+void store_variable_if_valid(s_config& server, std::pair<std::string, std::string>& var)
+{
+    std::vector<std::map<std::string, void*> > srvVars;
+    std::vector<std::map<std::string, void*> >::iterator i;
+    std::map<std::string, void*>::iterator it;
+
+    srvVars.push_back(server.port);
+    srvVars.push_back(server.host);
+    srvVars.push_back(server.root);
+    srvVars.push_back(server.indexes);
+    srvVars.push_back(server.srvNames);
+    srvVars.push_back(server.errPages);
+    srvVars.push_back(server.maxBodySize);
+    srvVars.push_back(server.location);
+
+    for(i = srvVars.begin(); i != srvVars.end(); i++)
+    {
+        it = (*i).begin();
+        if (it->first == var.first)
+        {
+            it->second = allocate_the_appropriate_type(var.first);
+            break;
+        }
+    }
+    if (var.second.empty())
+    {
+        delete it->second;
+        throw std::invalid_argument(var.first + ": no given value");
+    }
+    give_attr_value(server, var.second);
+}
+
+void server_block(std::string& file)
 {
     s_config server;
-    attribute_initializer(server);
-    std::string line;
-    std::string rest;
-    std::getline(file, line);
+    attribute_initializer(server); // filling the server structure
     size_t i = -1;
-    while(++i < line.size() && (std::isspace(line.at(i)) || line.at(i) == '}' ));
-    line = line.substr(i, line.size() - i);
-    i = -1;
-    while(++i < line.size() && line.at(i) != '{' && line.at(i) != '\n');
-    rest = line.substr(i, line.size() - i);
-    line = line.substr(0, i);
-    if (line != "server")
-    {
-        delete &file;
-        throw std::invalid_argument("config file requires a server block: \nserver\n{\n  ...\n}");
-    }
-    std::stack<char> brackets;
+    while(++i < file.size() && std::isspace(file.at(i)));
+    file = file.substr(i, file.size() - i); // trimming whitespaces at the beginning 
     std::string attr;
-    for(size_t i = 0; i < rest.length(); i++)
+    std::string value;
+    std::vector<char> brackets;
+    i = 0;
+    while(i < file.size() && file.at(i) != ' ' && file.at(i) != '{' && file.at(i) != '\n')
     {
-        if (rest.at(i) == '{')
-            brackets.push(rest.at(i));
-        else
-        {
-            if (rest.at(i) != ' ' && rest.at(i) != '}')
-                attr.push_back(rest.at(i));
-            else
-            
-        }
-
+        attr.push_back(file.at(i));
+        i++;
     }
+    if (attr != server.server)
+        throw std::invalid_argument("config file requires a server block: \nserver\n{\n  ...\n}");
+    i--;
+    while(++i < file.size() && file.at(i) == '\n');
+    file = file.substr(i, file.size() - i);
+    attr = "";
+    value = "";
+    i = 0;
+    size_t j = 0;
+    while(i < file.length())
+    {
+        if (file.at(i) == '{')
+            brackets.push_back(file.at(i));
+        else if (file.at(i) == '}')
+        {
+            if (brackets.empty())
+                throw std::invalid_argument("no opening bracket to be close");
+            brackets.pop_back();
+        }
+        else if (file.at(i) != ' ' && file.at(i) != '\n')
+        {
+            while(file.at(i) != ' ' && file.at(i) != '\n')
+            {
+                attr.push_back(file.at(i));
+                i++;
+            }
+            while(file.at(i) == ' ')
+                i++;
+            j = i;
+            while(file.at(i++) != '\n');
+            value = file.substr(j, i - j - 1);
+            std::pair<std::string, std::string> var(attr, value);
+            store_variable_if_valid(server, var);
+            attr = "";
+            value = "";
+        }
+        i++;
+    }
+    if (!brackets.empty())
+        throw std::invalid_argument("opened bracket no closed");
 }
 
 void config_parser(std::ifstream& file)
 {
     std::string line;
     std::getline(file, line);
-
 }
 
 int main(int argc, char** argv)
@@ -97,9 +206,8 @@ int main(int argc, char** argv)
     {
         if (argc != 2)
             throw std::invalid_argument("./HyperVaulted <config_file>");
-        std::ifstream& file = config_reader(argv);
-        server_block(file);
-        delete &file;
+        std::string buffer = config_reader(argv);
+        server_block(buffer);
     }
     catch(const std::exception& e)
     {
