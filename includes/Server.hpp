46c5c656f55a44d56162781209dac793b60e2adb
xTyranT <oufer.kamal@gmail.com>
2024-02-01 18:02:21 +0100

    created the 'TyranT' branch to keep my work seperated and at the same time keeping the mastyer bbranch for merge only purposes

---
+++ includes/Server.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "webserv.hpp"
+
+class Location
+{
+    public:
+        std::string path;
+        std::vector<std::string> methods;
+        std::string root;
+        std::vector<std::string> indexes;
+        bool autoIndex;
+        bool upload;
+        bool cgi;
+        std::string uploadPath;
+        std::vector<std::string> cgiPaths;
+        std::map<int, std::string> ret;
+        Location(void);
+        void checkAndStoreLocationAttributes(std::vector<std::string> attr);
+        void checkNecessaryAttributes(void);
+        ~Location(void);
+
+};
+
+class Server
+{
+    public:
+        int port;
+        std::string host;
+        std::string root;
+        std::vector<std::string> indexes;
+        std::vector<std::string> srvNames;
+        std::map<int, std::string> errPages;
+        long maxBodySize;
+        std::vector<Location> location;
+        std::vector<Location>::iterator l_i;
+        Server(void);
+        void defaultErrorPages(void);
+        void checkNecessaryAttributes(void);
+        void printServerAttributes(void);
+        void checkAndStoreServerAttributes(std::vector<std::string> attr, std::ifstream& file);
+        void serverBlock(std::ifstream& file);
+        bool empty(void);
+        ~Server(void);
+};
+
+std::vector<Server> getAvailableServers(std::ifstream& file);
